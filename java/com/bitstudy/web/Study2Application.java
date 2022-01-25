cece4f2efb3d63d69c863967826ac4bc9db088a7
ChoHyunGook <whgusrnr7795@gmail.com>
2022-01-25 11:15:50 +0900

    2022-01-25 11:15 Study2

---
+++ java/com/bitstudy/web/Study2Application.java
@@ -0,0 +1,13 @@
+package com.bitstudy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study2Application.class, args);
+    }
+
+}
