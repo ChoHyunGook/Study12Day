1bf80217d5bcddc4c1899ea55ebb762b57e1a401
ChoHyunGook <whgusrnr7795@gmail.com>
2022-02-10 20:50:13 +0900

    Study11Day

--- java/com/bitstudy/web/quiz/service/Feb10ServiceImpl.java
+++ java/com/bitstudy/web/quiz/service/Feb10ServiceImpl.java
@@ -42,8 +42,48 @@ public class Feb10ServiceImpl implements Feb10Service{
     public void magicSquare() {
         double m =Math.random();
         int[][] magicSquare = new int[(int) m][(int) m];
+        if(m%2==1){
+            int x = 0, y = (int) (m/2);
+            magicSquare[x][y] = 1;
+
+            for(int i = 1; i < m*m; i++)
+            {
+                if((i+1)%m == 1){
+                    x++;
+                    if(x == m)
+                        x = 0;
+                    magicSquare[x][y] = i+1;
+                }else{
+                    x--;
+                    if(x == -1)
+                        x = (int) (m-1);
+
+                    y--;
+                    if(y == -1)
+                        y = (int) (m-1);
+
+                    magicSquare[x][y] = i+1;
+                }
+
+            }
+
+            for(int i = 0 ; i < m; i++)
+            {
+                for(int j = 0 ; j < m; j++)
+                    System.out.print(magicSquare[i][j] + " " );
+                System.out.println();
+            }
+
+
+        }else if(m%2==0){
+            
+        }
+
+
 
     }
+
+
 
     @Override
     public void zigzag() {
